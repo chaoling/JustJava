65cea9344a34a076721ea55500998254a213f80b
chaoling <ling.chao@gmail.com>
2016-09-05 16:09:01 -0700

    step7: local vs globle variable

--- app/src/main/java/com/lucassociates/chaol/justjava/MainActivity.java
+++ app/src/main/java/com/lucassociates/chaol/justjava/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends ActionBarActivity {
+    private int mQuantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,28 +27,27 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int numberOfCoffees = 2;
+        //int numberOfCoffees = 2;
         int unitPrice = 5;
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * unitPrice);
+        display(mQuantity);
+        displayPrice(mQuantity * unitPrice);
     }
 
     /**
      * This method is called when the quantitiy increment button is clicked
      */
     public void increment(View view) {
-        int quantity = 2;
-        quantity = quantity + 1;
-        display(quantity);
+        //int quantity = 2;
+        mQuantity = mQuantity + 1;
+        display(mQuantity);
     }
 
     /**
      * This method is called when the quantity decrement button is clicked
      */
     public void decrement(View view) {
-        int quantity = 2;
-        quantity = quantity - 1;
-        display(quantity);
+        mQuantity = mQuantity - 1;
+        display(mQuantity);
     }
 
     /**
